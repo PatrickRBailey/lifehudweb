ac19a6d5ebe90bd8d696dd42727fcb5f419433b5
Patrick Bailey <patrickbailey13@gmail.com>
2018-02-03 23:11:10 -0800

    basic setup for project

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LifeHud from './components/LifeHud';
+
+ReactDOM.render(<LifeHud />, document.getElementById('app'));
+
---
+++ src/components/LifeHud.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LifeHud extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( <h1>This is the lifehud app</h1> )
+  }
+}
+ 
+export default LifeHud;
